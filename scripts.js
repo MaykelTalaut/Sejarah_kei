5f29456b377e38db4dcaf39b3c3c6935de9f5dbc
M_kei <talautmaykel@gmail.com>
2025-01-18 19:59:35 +0900

    Add files via upload

---
+++ scripts.js
@@ -0,0 +1,6 @@
+// Contoh: Mengaktifkan efek hamburger menu
+const nav = document.querySelector('nav');
+    nav.classList.toggle('active');
+document.addEventListener('DOMContentLoaded', function() {
+    console.log('Page loaded');
+});
